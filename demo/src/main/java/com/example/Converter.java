0ce2c4fb712a0deaeef8103f3c94512485a4ea64
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-06-08 15:20:29 -0400

    Refactor Converter conversion logic and improve readability

--- demo/src/main/java/com/example/Converter.java
+++ demo/src/main/java/com/example/Converter.java
@@ -14,26 +14,22 @@ public class Converter {
         romanMap.put('D', 500);
         romanMap.put('M', 1000);
 
-        if(value.length() >= 2) {
-            int total = 0;
-            for (int i = 0; i < value.length(); i++) {
-                char currentLetter = value.charAt(i);
-                if(i+1 < value.length()) {
-                    char nextLetter = value.charAt(i+1);
-                    int currentValue = romanMap.get(currentLetter);
+        int total = 0;
+        for (int i = 0; i < value.length(); i++) {
+            char currentLetter = value.charAt(i);
+            int currentValue = romanMap.get(currentLetter);
 
-                    if (currentValue < romanMap.get(nextLetter)) {
-                        total -= currentValue;
-                    } else {
-                        total += currentValue;
-                    }
+            if (i + 1 < value.length()) {
+                char nextLetter = value.charAt(i + 1);
+                if (currentValue < romanMap.get(nextLetter)) {
+                    total -= currentValue;
                 } else {
-                    total += romanMap.get(currentLetter);
+                    total += currentValue;
                 }
+            } else {
+                total += currentValue;
             }
-            return total;
-        } else {
-            return romanMap.get(value.charAt(0));
         }
+        return total;
     }
 }
