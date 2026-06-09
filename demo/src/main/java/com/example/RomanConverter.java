07410cb0c2d28e2ea61ff2a90bc127d483531b69
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-06-09 09:03:59 -0400

    Added limit values and symbols to conversion

--- demo/src/main/java/com/example/RomanConverter.java
+++ demo/src/main/java/com/example/RomanConverter.java
@@ -42,8 +42,8 @@ public class RomanConverter {
 
     public String integerToRoman(int value) {
         String integerToRoman = "";
-        int[] values = {10, 9, 5, 4, 1};
-        String[] symbols = {"X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int index = 0;
 
         while(value > 0) {
